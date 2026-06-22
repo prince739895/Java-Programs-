bde9b8cb673f189e443fc9bed63ad4f0a2270851
india <prince739895@gmail.com>
2026-06-22 12:12:08 -0700

    commit

---
+++ src/If_Else/AbsoluteValue.java
@@ -0,0 +1,16 @@
+package If_Else;
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enetr the number :");
+        int n = sc.nextInt();
+        if( n<0)n = -n;
+        System.out.println(n);
+//        if (n>=0) System.out.println(n);
+//        else System.out.println(-n);
+    }
+}
